df85f86e4d2089c53c086cb3271239b737a8b023
ryanrubi <ryanrubi17@gmail.com>
2022-10-16 06:31:14 +0800

    portfolio updates

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -41,7 +41,7 @@ const NavBar = () => {
                 <button className={fixed ? "nav-btn fixed" : "nav-btn"} onClick={openNavBar}>≡</button>
                 <ul className={fixed ? "nav-ul fixed" : "nav-ul"} id="nav">
                     <li><Link to="home" spy={true} smooth={true} offset={0} duration={500} className={fixed ? "nav-li fixed" : "nav-li"} onClick={openNavBar}>Home</Link></li>
-                    <li><Link to="about" spy={true} smooth={true} offset={-50} duration={500} className={fixed ? "nav-li fixed" : "nav-li"} onClick={openNavBar}>About</Link></li>
+                    <li><Link to="about" spy={true} smooth={true} offset={40} duration={500} className={fixed ? "nav-li fixed" : "nav-li"} onClick={openNavBar}>About</Link></li>
                     <li><Link to="services" spy={true} smooth={true} offset={40} duration={500} className={fixed ? "nav-li fixed" : "nav-li"} onClick={openNavBar}>Services</Link></li>
                     <li><NavLink className={fixed ? "nav-li fixed" : "nav-li"} to="/project">Project</NavLink></li>
                 </ul>
